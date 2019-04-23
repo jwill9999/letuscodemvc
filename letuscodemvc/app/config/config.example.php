a708e60ceca367cb8485bc3238e5665ebb563df0
Jason Williams <jasonwilliams2308@gmail.com>
2019-04-23 23:34:29 +0100

    config example

---
+++ letuscodemvc/app/config/config.example.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', '');
+  define('DB_USER', '');
+  define('DB_PASS', '');
+  define('DB_NAME', '');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'localhost/letuscodemvc');
+  // Site Name
+  define('SITENAME', 'LetUsCode');
